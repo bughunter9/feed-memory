d27f76e5dd8509c28520c63d4e4ef42909467bdf
MANROOP PARMAR <manroopsingh120@gmail.com>
2020-12-07 00:46:54 +0530

    Save

---
+++ backend/routes/posts.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const express = require("express");
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+router.get("/", (req, res) => {
+    res.send({ response: "Server is up and running." }).status(200);
+  });
+
+export default router;
